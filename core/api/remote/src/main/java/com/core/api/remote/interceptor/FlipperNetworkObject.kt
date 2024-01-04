d67ee377d68d1bd53bf36ed15ef6359294167aaf
Sahan <sahan.senvar@outlook.com>
2024-01-05 01:44:44 +0300

    flipper has added
    Signed-off-by: Sahan <sahan.senvar@outlook.com>

---
+++ core/api/remote/src/main/java/com/core/api/remote/interceptor/FlipperNetworkObject.kt
@@ -0,0 +1,11 @@
+package com.core.api.remote.interceptor
+
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+
+/**
+ * @authot: Vahit Keskin
+ * creared on 20.01.2023
+ */
+object FlipperNetworkObject {
+    var networkFlipperPlugin: NetworkFlipperPlugin? = null
+}
