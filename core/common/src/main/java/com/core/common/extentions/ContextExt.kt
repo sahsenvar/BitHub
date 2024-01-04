5fd01c0dd4b092b2048cc31c8cb71db72c3bcbc7
Sahan <sahan.senvar@outlook.com>
2024-01-05 01:46:18 +0300

    added 2 extension file
    Signed-off-by: Sahan <sahan.senvar@outlook.com>

---
+++ core/common/src/main/java/com/core/common/extentions/ContextExt.kt
@@ -0,0 +1,16 @@
+package com.core.common.extentions
+
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+fun Context.isNetworkAvailable(): Boolean {
+    val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val network = connectivityManager.activeNetwork ?: return false
+    val capabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+    return capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+}
