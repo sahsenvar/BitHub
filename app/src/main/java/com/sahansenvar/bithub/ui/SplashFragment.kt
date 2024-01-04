ceecf5deb5c62e1adfbb7a820ea9f04b52f98674
Sahan <sahan.senvar@outlook.com>
2024-01-05 01:32:48 +0300

    Splash UI components have created
    Signed-off-by: Sahan <sahan.senvar@outlook.com>

--- app/src/main/java/com/sahansenvar/bithub/ui/SplashFragment.kt
+++ app/src/main/java/com/sahansenvar/bithub/ui/SplashFragment.kt
@@ -1,32 +1,38 @@
 package com.sahansenvar.bithub.ui
 
-import androidx.lifecycle.ViewModelProvider
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.sahansenvar.bithub.R
+import android.util.Log
+import androidx.navigation.fragment.findNavController
+import com.core.common.base.BaseFragment
+import com.sahansenvar.bithub.databinding.FragmentSplashBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class SplashFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = SplashFragment()
+class SplashFragment : BaseFragment<FragmentSplashBinding, SplashState, SplashIntent, SplashViewModel>() {
+    override val viewModel: SplashViewModel by viewModel()
+    private val directions by lazy { SplashFragmentDirections }
+    override fun onCreationFinished() {
+        Log.i(TAG, "woke up")
+        viewModel.sendIntent(SplashIntent.CheckUserLoginInfo)
     }
 
-    private lateinit var viewModel: SplashViewModel
+    override fun initListeners() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SplashViewModel::class.java)
-        // TODO: Use the ViewModel
+
+    override suspend fun observeState(state: SplashState) {
+        when (state) {
+            is SplashState.Loading -> {
+                Log.e(TAG, "loginstate: exception", )
+                setLoadingVisibility(state.isEnable)
+            }
+            is SplashState.UserLoggedIn -> {
+                findNavController().navigate(directions.toHomeFragment())
+            }
+            is SplashState.UserNotLoggedIn -> {
+                findNavController().navigate(directions.toLoginRegisterFragment())
+            }
+        }
+
     }
 
 }
--- app/src/main/java/com/sahansenvar/bithub/ui/SplashViewModel.kt
+++ app/src/main/java/com/sahansenvar/bithub/ui/SplashViewModel.kt
@@ -1,7 +1,40 @@
 package com.sahansenvar.bithub.ui
 
-import androidx.lifecycle.ViewModel
+import PreferenceHelper
+import android.util.Log
+import androidx.lifecycle.viewModelScope
+import com.core.common.base.BaseViewModel
+import com.core.common.extentions.onError
+import com.core.common.extentions.onFound
+import kotlinx.coroutines.launch
 
-class SplashViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class SplashViewModel(
+    private val preferenceHelper: PreferenceHelper
+) : BaseViewModel<SplashState, SplashIntent>() {
+
+    override suspend fun handleIntent(intent: SplashIntent) {
+        when (intent) {
+            is SplashIntent.CheckUserLoginInfo -> checkUserLoginStatus()
+        }
+    }
+
+
+    private fun checkUserLoginStatus() {
+        viewModelScope.launch {
+            preferenceHelper.findUserToken().onFound { accessToken ->
+                if (accessToken.isEmpty()){
+                    Log.i(TAG, "checkUserLoginStatus: accessToken bulunamadı Login'e yönelndiriliyor")
+                    _state.send(SplashState.UserNotLoggedIn)
+                }
+                else{
+                    Log.i(TAG, "checkUserLoginStatus: accessToken bulundu: $accessToken. Home' gidiliyor")
+                    _state.send(SplashState.UserLoggedIn)
+                }
+            }.onError {
+                Log.i(TAG, "checkUserLoginStatus: accessToken bulunamadı Login'e yönelndiriliyor")
+                _state.send(SplashState.UserNotLoggedIn)
+            }
+        }
+    }
+
 }
